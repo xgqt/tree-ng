fac38f233631947a0f21ef7d3865a38ba98098d3
Maciej Barć <xgqt@protonmail.com>
2021-01-23 22:54:03 +0100

    src/*: add license; stylize
    Signed-off-by: Maciej Barć <xgqt@protonmail.com>

--- src/strverscmp.c
+++ src/strverscmp.c
@@ -1,25 +1,20 @@
-/* $Copyright: $
- * Copyright (c) 1996 - 2018 by Steve Baker (ice@mama.indstate.edu)
- * Copyright (c) 2020 by Maciej Barć (xgqt@protonmail.com)
- * All Rights reserved
- *
- * This program is free software; you can redistribute it and/or modify
+/*
+ * tree-ng is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
+
+ * tree-ng is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * Compare strings while treating digits characters numerically.
- * Copyright (C) 1997, 2002, 2005 Free Software Foundation, Inc.
- * This file is part of the libiberty library.
- * Contributed by Jean-François Bignolles <bignolle@ecoledoc.ibp.fr>, 1997.
+ * along with tree-ng.  If not, see <https://www.gnu.org/licenses/>.
+
+ * Copyright (c) 1996 - 2018 by Steve Baker (ice@mama.indstate.edu)
+ * Copyright (c) 2020-2021, Maciej Barć (xgqt@protonmail.com)
+ * Licensed under the GNU GPL v3 License
 
  * Libiberty is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -32,61 +27,63 @@
  * Lesser General Public License for more details.
  */
 
-/* #include "libiberty.h" */
-/* #include "safe-ctype.h" */
+
+// #include "libiberty.h"
+// #include "safe-ctype.h"
 #include <ctype.h>
 
-/* 
-@deftypefun int strverscmp (const char *@var{s1}, const char *@var{s2})
-The @code{strverscmp} function compares the string @var{s1} against
-@var{s2}, considering them as holding indices/version numbers.  Return
-value follows the same conventions as found in the @code{strverscmp}
-function.  In fact, if @var{s1} and @var{s2} contain no digits,
-@code{strverscmp} behaves like @code{strcmp}.
 
-Basically, we compare strings normally (character by character), until
-we find a digit in each string - then we enter a special comparison
-mode, where each sequence of digits is taken as a whole.  If we reach the
-end of these two parts without noticing a difference, we return to the
-standard comparison mode.  There are two types of numeric parts:
-"integral" and "fractional" (those  begin with a '0'). The types
-of the numeric parts affect the way we sort them:
+/*
+  @deftypefun int strverscmp (const char *@var{s1}, const char *@var{s2})
+  The @code{strverscmp} function compares the string @var{s1} against
+  @var{s2}, considering them as holding indices/version numbers.  Return
+  value follows the same conventions as found in the @code{strverscmp}
+  function.  In fact, if @var{s1} and @var{s2} contain no digits,
+  @code{strverscmp} behaves like @code{strcmp}.
 
-@itemize @bullet
-@item
-integral/integral: we compare values as you would expect.
+  Basically, we compare strings normally (character by character), until
+  we find a digit in each string - then we enter a special comparison
+  mode, where each sequence of digits is taken as a whole.  If we reach the
+  end of these two parts without noticing a difference, we return to the
+  standard comparison mode.  There are two types of numeric parts:
+  "integral" and "fractional" (those  begin with a '0'). The types
+  of the numeric parts affect the way we sort them:
 
-@item
-fractional/integral: the fractional part is less than the integral one.
-Again, no surprise.
+  @itemize @bullet
+  @item
+  integral/integral: we compare values as you would expect.
 
-@item
-fractional/fractional: the things become a bit more complex.
-If the common prefix contains only leading zeroes, the longest part is less
-than the other one; else the comparison behaves normally.
-@end itemize
+  @item
+  fractional/integral: the fractional part is less than the integral one.
+  Again, no surprise.
 
-@smallexample
-strverscmp ("no digit", "no digit")
-    @result{} 0    // @r{same behavior as strcmp.}
-strverscmp ("item#99", "item#100")
-    @result{} <0   // @r{same prefix, but 99 < 100.}
-strverscmp ("alpha1", "alpha001")
-    @result{} >0   // @r{fractional part inferior to integral one.}
-strverscmp ("part1_f012", "part1_f01")
-    @result{} >0   // @r{two fractional parts.}
-strverscmp ("foo.009", "foo.0")
-    @result{} <0   // @r{idem, but with leading zeroes only.}
-@end smallexample
+  @item
+  fractional/fractional: the things become a bit more complex.
+  If the common prefix contains only leading zeroes, the longest part is less
+  than the other one; else the comparison behaves normally.
+  @end itemize
 
-This function is especially useful when dealing with filename sorting,
-because filenames frequently hold indices/version numbers.
-@end deftypefun
+  @smallexample
+  strverscmp ("no digit", "no digit")
+  @result{} 0    // @r{same behavior as strcmp.}
+  strverscmp ("item#99", "item#100")
+  @result{} <0   // @r{same prefix, but 99 < 100.}
+  strverscmp ("alpha1", "alpha001")
+  @result{} >0   // @r{fractional part inferior to integral one.}
+  strverscmp ("part1_f012", "part1_f01")
+  @result{} >0   // @r{two fractional parts.}
+  strverscmp ("foo.009", "foo.0")
+  @result{} <0   // @r{idem, but with leading zeroes only.}
+  @end smallexample
+
+  This function is especially useful when dealing with filename sorting,
+  because filenames frequently hold indices/version numbers.
+  @end deftypefun
 
 */
 
 /* states: S_N: normal, S_I: comparing integral part, S_F: comparing
-           fractional parts, S_Z: idem but with leading Zeroes only */
+   fractional parts, S_Z: idem but with leading Zeroes only */
 #define  S_N    0x0
 #define  S_I    0x4
 #define  S_F    0x8
@@ -104,69 +101,69 @@ because filenames frequently hold indices/version numbers.
 int
 strverscmp (const char *s1, const char *s2)
 {
-  const unsigned char *p1 = (const unsigned char *) s1;
-  const unsigned char *p2 = (const unsigned char *) s2;
-  unsigned char c1, c2;
-  int state;
-  int diff;
+    const unsigned char *p1 = (const unsigned char *) s1;
+    const unsigned char *p2 = (const unsigned char *) s2;
+    unsigned char c1, c2;
+    int state;
+    int diff;
 
-  /* Symbol(s)    0       [1-9]   others  (padding)
-     Transition   (10) 0  (01) d  (00) x  (11) -   */
-  static const unsigned int next_state[] =
-    {
-      /* state    x    d    0    - */
-      /* S_N */  S_N, S_I, S_Z, S_N,
-      /* S_I */  S_N, S_I, S_I, S_I,
-      /* S_F */  S_N, S_F, S_F, S_F,
-      /* S_Z */  S_N, S_F, S_Z, S_Z
-    };
+    /* Symbol(s)    0       [1-9]   others  (padding)
+       Transition   (10) 0  (01) d  (00) x  (11) -   */
+    static const unsigned int next_state[] =
+        {
+            /* state    x    d    0    - */
+            /* S_N */  S_N, S_I, S_Z, S_N,
+            /* S_I */  S_N, S_I, S_I, S_I,
+            /* S_F */  S_N, S_F, S_F, S_F,
+            /* S_Z */  S_N, S_F, S_Z, S_Z
+        };
 
-  static const int result_type[] =
-    {
-      /* state   x/x  x/d  x/0  x/-  d/x  d/d  d/0  d/-
-                 0/x  0/d  0/0  0/-  -/x  -/d  -/0  -/- */
+    static const int result_type[] =
+        {
+            /* state   x/x  x/d  x/0  x/-  d/x  d/d  d/0  d/-
+               0/x  0/d  0/0  0/-  -/x  -/d  -/0  -/- */
 
-      /* S_N */  CMP, CMP, CMP, CMP, CMP, LEN, CMP, CMP,
-                 CMP, CMP, CMP, CMP, CMP, CMP, CMP, CMP,
-      /* S_I */  CMP, -1,  -1,  CMP, +1,  LEN, LEN, CMP,
-                 +1,  LEN, LEN, CMP, CMP, CMP, CMP, CMP,
-      /* S_F */  CMP, CMP, CMP, CMP, CMP, LEN, CMP, CMP,
-                 CMP, CMP, CMP, CMP, CMP, CMP, CMP, CMP,
-      /* S_Z */  CMP, +1,  +1,  CMP, -1,  CMP, CMP, CMP,
-                 -1,  CMP, CMP, CMP
-    };
+            /* S_N */  CMP, CMP, CMP, CMP, CMP, LEN, CMP, CMP,
+            CMP, CMP, CMP, CMP, CMP, CMP, CMP, CMP,
+            /* S_I */  CMP, -1,  -1,  CMP, +1,  LEN, LEN, CMP,
+            +1,  LEN, LEN, CMP, CMP, CMP, CMP, CMP,
+            /* S_F */  CMP, CMP, CMP, CMP, CMP, LEN, CMP, CMP,
+            CMP, CMP, CMP, CMP, CMP, CMP, CMP, CMP,
+            /* S_Z */  CMP, +1,  +1,  CMP, -1,  CMP, CMP, CMP,
+            -1,  CMP, CMP, CMP
+        };
 
-  if (p1 == p2)
-    return 0;
+    if (p1 == p2)
+        return 0;
 
-  c1 = *p1++;
-  c2 = *p2++;
-  /* Hint: '0' is a digit too.  */
-  state = S_N | ((c1 == '0') + (isdigit (c1) != 0));
+    c1 = *p1++;
+    c2 = *p2++;
+    /* Hint: '0' is a digit too.  */
+    state = S_N | ((c1 == '0') + (isdigit (c1) != 0));
 
-  while ((diff = c1 - c2) == 0 && c1 != '\0')
-    {
-      state = next_state[state];
-      c1 = *p1++;
-      c2 = *p2++;
-      state |= (c1 == '0') + (isdigit (c1) != 0);
-    }
+    while ((diff = c1 - c2) == 0 && c1 != '\0')
+        {
+            state = next_state[state];
+            c1 = *p1++;
+            c2 = *p2++;
+            state |= (c1 == '0') + (isdigit (c1) != 0);
+        }
 
-  state = result_type[state << 2 | (((c2 == '0') + (isdigit (c2) != 0)))];
+    state = result_type[state << 2 | (((c2 == '0') + (isdigit (c2) != 0)))];
 
-  switch (state)
-    {
-    case CMP:
-      return diff;
-      
-    case LEN:
-      while (isdigit (*p1++))
-	if (!isdigit (*p2++))
-	  return 1;
-      
-      return isdigit (*p2) ? -1 : diff;
-      
-    default:
-      return state;
-    }
+    switch (state)
+        {
+        case CMP:
+            return diff;
+
+        case LEN:
+            while (isdigit (*p1++))
+                if (!isdigit (*p2++))
+                    return 1;
+
+            return isdigit (*p2) ? -1 : diff;
+
+        default:
+            return state;
+        }
 }
--- src/tree.h
+++ src/tree.h
@@ -1,24 +1,26 @@
-/* $Copyright: $
- * Copyright (c) 1996 - 2018 by Steve Baker (ice@mama.indstate.edu)
- * Copyright (c) 2020 by Maciej Barć (xgqt@protonmail.com)
- * All Rights reserved
- *
- * This program is free software; you can redistribute it and/or modify
+/*
+ * tree-ng is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
+
+ * tree-ng is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * along with tree-ng.  If not, see <https://www.gnu.org/licenses/>.
+
+ * Copyright (c) 1996 - 2018 by Steve Baker (ice@mama.indstate.edu)
+ * Copyright (c) 2020-2021, Maciej Barć (xgqt@protonmail.com)
+ * Licensed under the GNU GPL v3 License
  */
 
+
+
 #define _GNU_SOURCE
+
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -34,6 +36,8 @@
 #include <limits.h>
 #include <pwd.h>
 #include <grp.h>
+
+
 #ifdef __EMX__  /* for OS/2 systems */
 #  define INCL_DOSFILEMGR
 #  define INCL_DOSNLS
